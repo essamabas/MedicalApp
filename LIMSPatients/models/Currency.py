79fa7454266f518af61ba93232bfbec7dcc5a1fb
qegabbe <qegabbe@outlook.de>
2015-09-18 22:41:27 +0200

    V 0.0.2.3

--- LIMSPatients/models/Currency.py
+++ LIMSPatients/models/Currency.py
@@ -2,17 +2,20 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from MedicalApp import settings
-
+import requests
 
 # Create your models here.
 class Currency(models.Model):
     class Meta(object):
         verbose_name = _('currency')
         verbose_name_plural = _('currencies')
-    name = models.CharField(_("name"), max_length=3, default=settings.DEFAULT_CURRENCY_USED)
+    name = models.CharField(_("name"), max_length=3, default=settings.DEFAULT_CURRENCY_USED, unique=True)
     info = models.CharField(_("info"), max_length=255, blank=True)
 
     def _get_exchange_rate(self):
         "Returns the person's full name."
+        r = requests.get('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.xchange where pair in ("USDEUR")&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys')
+        #if(r.status_code==200):
+            #r.content
         return '%s %s' % (self.first_name, self.last_name)
 
