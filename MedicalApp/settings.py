157884908d260921352a188990f9e94c7f0cb529
qegabbe <qegabbe@gmail.com>
2015-10-29 12:45:39 -0400

    V 0.0.2.4

--- MedicalApp/settings.py
+++ MedicalApp/settings.py
@@ -127,6 +127,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         #'rest_framework.permissions.IsAdminUser',
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions',
     ),
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
