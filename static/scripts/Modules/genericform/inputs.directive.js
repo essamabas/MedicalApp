0db4fbe9919eebfad0ab9300acf9e5f8412a03db
qegabbe <qegabbe@gmail.com>
2015-11-19 07:39:35 -0500

    V 0.0.2.5

--- static/scripts/Modules/genericform/inputs.directive.js
+++ static/scripts/Modules/genericform/inputs.directive.js
@@ -15,7 +15,43 @@ angular.module('sbAdminApp',['ngMessages',{
 // --------------------------------------------
 // Directives 
 // ----------
-function formInput ($compile,BaseUrl) {
+// Use ngModel to link Model to ngMessages - as described in: http://jsfiddle.net/12sf82p3/
+function formInput($compile,BaseUrl) {
+    return {
+        restrict: 'E',
+        template:
+						'<div class="form-group has-feedback ng-scope">' +
+    				'<label class="control-label" for="ctrl.inputname" >  </label> ' + 			
+            '<input type="text" ng-model="ctrl.model" name="ctrl.inputname" />' + 
+            '<div ng-messages="ctrl.$error" role="alert" ng-transclude>' +
+            '</div>',
+        transclude: true,
+        scope: {},
+        require: ['ngModel', 'formInput'],
+        link: function(scope, elem, attrs, ctrls) {
+            var ngModel = ctrls[0];
+            var formInput = ctrls[1];
+            formInput.inputname = attrs.inputname;
+            formInput.setModel(ngModel);
+        },
+        controllerAs: 'ctrl',
+        controller: function($scope) {
+            var self = this;
+            this.model = null;
+            this.setModel = function(ngModel) {
+                this.$error = ngModel.$error;
+                ngModel.$render = function() {
+                    self.model = ngModel.$viewValue;
+                };
+                $scope.$watch('ctrl.model', function(newval) {
+                    ngModel.$setViewValue(newval);
+                });
+            };
+        }
+    };
+}
+
+function formInput2 ($compile,BaseUrl) {
   return {
     templateUrl: BaseUrl.url+'scripts/Modules/genericform/formInput.html',
     restrict: 'E, A, C',
@@ -29,10 +65,11 @@ function formInput ($compile,BaseUrl) {
 					// your code goes here
 					scope.opts = attrs;
 					//Compile element - could be scope.$parent
-					$compile(element.contents())(scope);					
+					$compile(element.contents())(scope);
 				}
 			});
 			
+			/*
       scope.$watch(attrs.value,function(newValue,oldValue){
         //check new value to be what you expect.
         if (newValue){           
@@ -41,7 +78,8 @@ function formInput ($compile,BaseUrl) {
             //Compile element - could be scope.$parent
             $compile(element.contents())(scope);
         }
-      });      
+      });
+			*/
     }
   };
 }
