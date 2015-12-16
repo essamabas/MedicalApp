9fa3308d0152f4714e561f1d28ee15a7610c4aaa
qegabbe <qegabbe@outlook.de>
2015-12-16 13:24:56 +0100

    Update install_ubuntu.sh

--- env_install/install_ubuntu.sh
+++ env_install/install_ubuntu.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sudo apt-get update
+# python 3
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
+sudo pip install virtualenv
+# Activate Virtual-ENV
+virtualenv my_py3 --python=/usr/bin/python3
+source my_py3/bin/activate
+# deactive
 sudo apt-get install git
 sudo apt-get install nodejs-legacy
 sudo npm install -g bower
